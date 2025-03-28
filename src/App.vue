de2794a88dcf758106ea0602e484895a5bbe70d3
vitormanoelssantana <vitormanoels.santana@gmail.com>
2025-03-28 09:30:24 -0300

    exercicio

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 
-const contador = ref(0);
+let contador = ref(0);
+
 
 function incrementar() {
   contador.value++
@@ -9,11 +10,21 @@ function incrementar() {
 function decrementar() {
   contador.value--
 }
+function resetar() {
+  contador = 0;
+}
+if (contador = 1) {
+  contador = 0;
+} else {
+  contador.value--
+}
+
 </script>
 <template>
   <h1>Contador</h1>
   <p>Contador: {{ contador }}</p>
   <button @click="decrementar"> - </button>
   <button @click="incrementar"> + </button>
+  <button @click="resetar"> Resetar </button>
 </template>
 <style scoped ></style>
