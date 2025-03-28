1c78c2ea2267a81c6bf9c908d813c9f323a3fd0e
vitormanoelssantana <vitormanoels.santana@gmail.com>
2025-03-28 09:05:00 -0300

    começo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+function incrementar() {
+  contador.value++
+}
+function decrementar() {
+  contador.value--
+}
+</script>
+<template>
+  <h1>Contador</h1>
+  <p>Contador: {{ contador }}</p>
+  <button @click="decrementar"> - </button>
+  <button @click="incrementar"> + </button>
+</template>
+<style scoped ></style>
